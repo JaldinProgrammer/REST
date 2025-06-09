911ab37fdf6f2c3286c54a07ecbb276230523d20
Daniel Merida <daniel.merida@assuresoft.com>
2025-06-09 18:57:17 -0400

    Add comments of bad practices

--- src/routes/v1/post.routes.ts
+++ src/routes/v1/post.routes.ts
@@ -17,29 +17,29 @@ router.get('/',
   postController.getAllPosts.bind(postController)
 );
 
-router.post('/getPostById' , async function(req, res) {
-    const postId = parseInt(req.body.postId);
+router.post('/getPostById' , async function(req, res) { //usar Post para una peticion de lectura de datos
+    const postId = req.body.postId;   // no  se valida o transforma postId a number
     const post = await postModel.findById(postId);
     if (!post) {
-        res.status(200).json(null);
+        res.status(200).json(null); //retornar un status 200 aunque no se encuentra el reucrso
         return;
     }
 
-    res.json({
-        result: post,
+    res.json({  //no retornar el codigo de estado
+        result: post,   //Cambio de formato de los tipos de respuesta
         links: []
     });
 });
 
 router.get('/deletePost' , async function(req, res) {
-    const postId = req.query.postId?.toString() || "0";
-    const post = await postModel.findById(parseInt(postId));
+    const postId = req.query.postId?.toString() || "0"; // eliminando toString y parse int se aplica malas practicas
+    const post = await postModel.findById(parseInt(postId));    // solo lo agregamos para que compile xD
     if (!post) {
-        res.status(200).json(null);
+        res.status(200).json(null); // retornar 200 aunque el recurso no exista
         return;
     }
-    await postModel.deletePostById(parseInt(postId));
-    res.send('Deleted');
+    await postModel.deletePostById(parseInt(postId));   //no existe manejo de errores
+    res.send('Deleted');    //retorno de respuesta sin estructura ni codigo de estado
 });
 
 export default router;
